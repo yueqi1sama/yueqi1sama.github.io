9184ba588895dcf6c01d77a0838d26cb338a2c87
huxin-uki <3574340842@qq.com>
2026-02-01 14:33:02 +0800

    Site updated: 2026-02-01 14:33:02

--- js/clickjs/fireworks.js
+++ js/clickjs/fireworks.js
@@ -1,13 +1,5 @@
 function initfirework() {
 
-// 创建 Canvas 元素
-    var div = document.createElement("canvas");
-    div.className = "fireworks";
-    div.style = "position:fixed;left:0;top:0;z-index:99999999;pointer-events:none;";
-
-    // 将 canvas 添加到 body
-    document.body.appendChild(div);
-    // 初始化变量
 
     function updateCoords(e) {
         pointerX = (e.clientX || e.touches[0].clientX) - canvasEl.getBoundingClientRect().left,
@@ -30,6 +22,26 @@ function initfirework() {
             a.radius = anime.random(16, 32),
             a.endPos = setParticuleDirection(a),
             a.draw = function () {
+                ctx.beginPath(),
+                    ctx.arc(a.x, a.y, a.radius, 0, 2 * Math.PI, !0),
+                    ctx.fillStyle = a.color,
+                    ctx.fill()
+            }
+            ,
+            a
+    }
+    function createCircle(e, t) {
+        var a = {};
+        return a.x = e,
+            a.y = t,
+            a.color = "#F00",
+            a.radius = .1,
+            a.alpha = .5,
+            a.lineWidth = 6,
+            a.draw = function () {
+                ctx.globalAlpha = a.alpha,
+                    ctx.beginPath(),
+                    ctx.arc(a.x, a.y, a.radius, 0, 2 * Math.PI, !0),
                     ctx.lineWidth = a.lineWidth,
                     ctx.strokeStyle = a.color,
                     ctx.stroke(),
@@ -38,6 +50,18 @@ function initfirework() {
             ,
             a
     }
+    function renderParticule(e) {
+        for (var t = 0; t < e.animatables.length; t++)
+            e.animatables[t].target.draw()
+    }
+    function animateParticules(e, t) {
+        for (var a = createCircle(e, t), n = [], i = 0; i < numberOfParticules; i++)
+            n.push(createParticule(e, t));
+        anime.timeline().add({
+            targets: n,
+            x: function (e) {
+                return e.endPos.x
+            },
             y: function (e) {
                 return e.endPos.y
             },
@@ -46,6 +70,19 @@ function initfirework() {
             easing: "easeOutExpo",
             update: renderParticule
         }).add({
+            targets: a,
+            radius: anime.random(80, 160),
+            lineWidth: 0,
+            alpha: {
+                value: 0,
+                easing: "linear",
+                duration: anime.random(600, 800)
+            },
+            duration: anime.random(1200, 1800),
+            easing: "easeOutExpo",
+            update: renderParticule,
+            offset: 0
+        })
     }
     function debounce(fn, delay) {
         var timer
@@ -66,3 +103,41 @@ function initfirework() {
 	// 将 div 添加到 body 的末尾
 	document.body.appendChild(div);
 
+    var canvasEl = document.querySelector(".fireworks");
+    if (canvasEl) {
+        var ctx = canvasEl.getContext("2d")
+            , numberOfParticules = 30
+            , pointerX = 0
+            , pointerY = 0
+            , tap = "mousedown"
+            , colors = ["#FF1461", "#18FF92", "#5A87FF", "#FBF38C"]
+            , setCanvasSize = debounce(function () {
+                canvasEl.width = 2 * window.innerWidth,
+                    canvasEl.height = 2 * window.innerHeight,
+                    canvasEl.style.width = window.innerWidth + "px",
+                    canvasEl.style.height = window.innerHeight + "px",
+                    canvasEl.getContext("2d").scale(2, 2)
+            }, 500)
+            , render = anime({
+                duration: 1 / 0,
+                update: function () {
+                    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height)
+                }
+            });
+        document.addEventListener(tap, function (e) {
+            "sidebar" !== e.target.id && "toggle-sidebar" !== e.target.id && "A" !== e.target.nodeName && "IMG" !== e.target.nodeName && (render.play(),
+                updateCoords(e),
+                animateParticules(pointerX, pointerY))
+        }, !1),
+            setCanvasSize(),
+            window.addEventListener("resize", setCanvasSize, !1)
+    }
+}
+
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initfirework);
+}
+else {
+    initfirework() // DOMContentLoaded 已经发生
+}
